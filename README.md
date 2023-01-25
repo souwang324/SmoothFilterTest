eb020551ce51ba7a5e354bcbfbb50ca80d7d7965
souwang324 <33944965+souwang324@users.noreply.github.com>
2023-01-25 20:27:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SmoothFilterTest
+ SmoothFilterTest with CSharp Activiz x64 , Visual Studio 2017 Community, Windows10
---
+++ SmoothFilterTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kitware.VTK;
+
+
+namespace SmoothFilterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmoothFilterTest1();
+        }
+        public static void SmoothFilterTest1()
+        {
+            vtkSTLReader pSTLReader = vtkSTLReader.New();
+            pSTLReader.SetFileName("../../../../res/cow.stl");
+            pSTLReader.Update();
+
+            vtkWindowedSincPolyDataFilter smoothFilter = vtkWindowedSincPolyDataFilter.New();
+            smoothFilter.SetInputConnection(pSTLReader.GetOutputPort());
+            smoothFilter.SetNumberOfIterations(100);
+            smoothFilter.Update();
+
+            vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
+            mapper.SetInputConnection(smoothFilter.GetOutputPort());
+            //mapper.SetInput(normals.GetOutput());
+
+            vtkActor actor = vtkActor.New();
+            actor.SetMapper(mapper);
+
+            vtkRenderer renderer = vtkRenderer.New();
+            renderer.AddActor(actor);
+            renderer.SetBackground(.1, .2, .3);
+            renderer.ResetCamera();
+
+            vtkRenderWindow renderWin = vtkRenderWindow.New();
+            renderWin.AddRenderer(renderer);
+
+            vtkRenderWindowInteractor interactor = vtkRenderWindowInteractor.New();
+            interactor.SetRenderWindow(renderWin);
+
+            renderWin.Render();
+            interactor.Start();
+        }
+    }
+}
